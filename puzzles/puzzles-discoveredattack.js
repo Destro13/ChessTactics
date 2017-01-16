56533b1a2fff40b687c3d672c298c34958bf6be4
Nazariy Pavlyk <pavlyknazarij@gmail.com>
2017-01-16 21:37:09 +0200

    ChessTactics

---
+++ puzzles/puzzles-discoveredattack.js
@@ -0,0 +1,102 @@
+var puzzles = [
+
+ {
+  date: '03/10/14',
+  balP: '2',
+  balM: '2',
+  title: 'Black to move!',
+  start: '1r3rk1/3qbpp1/2n1pn1p/p2p4/1pPP4/3B1N2/PPQ2PPP/R1B2RK1 b - - 0 1',
+  moves: [
+   'd5-c4',
+   'd3-c4',
+   'c6-d4',
+   'f3-d4',
+   'd7-d4'
+  ]
+ },
+    
+    {
+  date: '08/10/14',
+  balP: '1',
+  balM: '1',
+  title: 'White to move!',
+  start: 'r3k2r/pp1nppbp/1qppbnp1/8/2PPP3/2NBBN2/PP1Q1PPP/R3K2R w - - 0 1',
+  moves: [
+   'd4-d5'
+  ]
+ },  
+
+   {
+  date: '09/10/14',
+  balP: '1',
+  balM: '1',
+  title: 'White to move!',
+  start: '3rn1k1/ppq2p1p/1n1pr1p1/2p1P3/2P2P2/1PQ1N3/PB1R2PP/3R3K w - - 0 1',
+  moves: [
+   'e5-d6',
+   'd8-d6',
+   'c3-h8'
+  ]
+ },
+    
+      {
+  date: '//',
+  balP: '1',
+  balM: '1',
+  title: 'Black to move! Mate in 3!',
+  start: '4r2k/R6B/5p2/1bBp3p/3P2p1/2n3P1/5P1P/6K1 b - - 0 1',
+  moves: [
+   'e8-e1',
+   'g1-g2',
+   'b5-f1',
+   'g2-g1',
+   'f1-h3'
+  ]
+ },
+    
+  {
+  date: '18/10/2014',
+  balP: '2',
+  balM: '2',
+  title: 'White to move!',
+  start: '3rr1k1/1p3p1n/p2pb1p1/2pN3p/2P1Pb2/1P3BPP/P2R1PK1/3R4 w - - 0 1',
+  moves: [
+   'd5-f4',
+   'h7-g5',
+   'd2-d6',
+   'd8-d6',
+   'd1-d6'
+  ]
+ },
+      
+  {
+  date: '06/12/2014',
+  balP: '1',
+  balM: '1',
+  title: 'White to move!',
+  start: '2r3k1/rp3pp1/2bp1n1p/1p2pP2/1PP1P3/2R1KB2/P3N1PP/R7 w - - 0 1',
+  moves: [
+   'c4-b5'
+  ]
+ },  
+    
+    {
+  date: '20/12/2014',
+  balP: '3',
+  balM: '3',
+  title: 'Black to move!',
+  start: 'r2qk2r/1b2bppp/p3p3/2pnN3/3p1P2/1P1P3P/PBPQN1P1/R3K2R b - - 0 1',
+  moves: [
+   'd5-e3',
+   'e5-c4',
+   'e3-g2',
+   'e1-d1',
+   'g2-e3',
+   'd1-c1',
+   'e3-c4',
+   'd3-c4',
+   'b7-h1',
+  ]
+ },     //#47 -> 7 
+];
+
---
+++ puzzles/puzzles-winningpawn.js
@@ -0,0 +1,295 @@
+var puzzles = [
+
+ {
+  date: '03/10/14',
+  balP: '1',
+  balM: '1',
+  title: 'Black to move!',
+  start: '3r3r/1p3ppp/pnk2b2/2pNp3/4P3/5P1P/P1P1K1P1/R5NR b - - 0 1',
+  moves: [
+   'b6-d5',
+   'e4-d5',
+   'd8-d5'
+  ]
+ },
+
+ {
+  date: '03/10/14',
+  balP: '2',
+  balM: '2',
+  title: 'Black to move!',
+  start: '1r3rk1/3qbpp1/2n1pn1p/p2p4/1pPP4/3B1N2/PPQ2PPP/R1B2RK1 b - - 0 1',
+  moves: [
+   'd5-c4',
+   'd3-c4',
+   'c6-d4',
+   'f3-d4',
+   'd7-d4'
+  ]
+ },
+
+ {
+  date: '05/10/14',
+  balP: '2',
+  balM: '2',
+  title: 'Black to move!',
+  start: '2kr3r/pp1nbpp1/q1p1p3/3pPnN1/3P2Q1/2N5/PPPB1P1P/R3K2R b - - 0 1',
+  moves: [
+   'h8-h4',
+   'g4-g2',
+   'f5-d4',
+   'g5-f7',
+   'd8-f8',
+   'g2-g7',
+   'd4-c2'
+  ]
+ }, 
+  
+ {
+  date: '05/10/14',
+  balP: '1',
+  balM: '1',
+  title: 'Black to move!',
+  start: '2k3r1/pp3p1p/2n1p1r1/3pN3/3P4/1BP3q1/PP1Q4/1K1R4 b - - 0 1',
+  moves: [
+   'c6-e5',
+   'd4-e5',
+   'g3-e5'
+  ]
+ },
+   
+ {
+  date: '07/10/14',
+  balP: '2',
+  balM: '2',
+  title: 'Black to move!',
+  start: 'r4rk1/pp3ppp/2n1p3/1N1pPn2/1q1P4/1P1Q1N2/P4PPP/R4RK1 b - - 0 1',
+  moves: [
+   'a7-a6',
+   'b5-c3',
+   'f5-d4',
+   'f3-d4',
+   'b4-d4',
+   'd3-d4',
+   'c6-d4'
+  ]
+ },
+    
+     {
+  date: '08/10/14',
+  balP: '1',
+  balM: '1',
+  title: 'Black to move!',
+  start: 'r3r1k1/1p3ppp/3b1q2/3p1n2/p2NpP2/P1P3Pb/1PB1R1NP/R1Q3K1 b - - 0 1',
+  moves: [
+   'f5-d4',
+   'c3-d4',
+   'f6-d4'
+  ]
+ },
+      
+  {
+  date: '11/10/14',
+  balP: '1',
+  balM: '1',
+  title: 'Black to move!',
+  start: 'r3rbk1/1p3bpp/1ppp1p2/2n5/P1PNP3/1P4PP/5PB1/1R3RK1 b - - 0 1',
+  moves: [
+   'c5-e4',
+   'g2-e4',
+   'e8-e4'
+  ]
+ }, 
+     
+  {
+  date: '18/10/2014',
+  balP: '2',
+  balM: '2',
+  title: 'White to move!',
+  start: '3rr1k1/1p3p1n/p2pb1p1/2pN3p/2P1Pb2/1P3BPP/P2R1PK1/3R4 w - - 0 1',
+  moves: [
+   'd5-f4',
+   'h7-g5',
+   'd2-d6',
+   'd8-d6',
+   'd1-d6'
+  ]
+ },  
+      
+      {
+  date: '18/10/2014',
+  balP: '2',
+  balM: '2',
+  title: 'White to move!',
+  start: '6k1/2n2p2/R5p1/2pBP2p/2Pr4/6PP/P4PK1/8 w - - 0 1',
+  moves: [
+   'a6-g6',
+   'g8-f8',
+   'g6-d6',
+   'c7-d5',
+   'c4-d5'
+  ]
+ },
+    
+      {
+  date: '26/10/2014',
+  balP: '2',
+  balM: '2',
+  title: 'Black to move!',
+  start: 'r3kb1r/pp2nppp/1qn1p3/3pP3/8/1PP2N2/P3BPPP/R1BQ1RK1 b - - 0 1',
+  moves: [
+   'e7-g6',
+   'c1-e3',
+   'b6-c7',
+   'e2-b5',
+   'g6-e5',
+   'f3-e5',
+   'c7-e5'
+  ]
+ },
+     
+  {
+  date: '29/11/2014',
+  balP: '2',
+  balM: '2',
+  title: 'White to move!',
+  start: 'rnbqk2r/ppp2ppp/5n2/3B4/1b2P3/2N2N2/PP3PPP/R1BQK2R w - - 0 1',
+  moves: [
+   'd1-a4',
+   'c7-c6',
+   'd5-f7',
+   'e8-f7',
+   'a4-b4'
+  ]
+ },     
+    
+  {
+  date: '06/12/2014',
+  balP: '1',
+  balM: '1',
+  title: 'White to move!',
+  start: '2r3k1/rp3pp1/2bp1n1p/1p2pP2/1PP1P3/2R1KB2/P3N1PP/R7 w - - 0 1',
+  moves: [
+   'c4-b5'
+  ]
+ },  
+
+      {
+  date: '11/12/2014',
+  balP: '1',
+  balM: '1',
+  title: 'White to move!',
+  start: 'rnbqkb1r/pp1p1p2/2p1pn1p/4N3/2PPP1p1/2N5/PP3PPP/R1BQKB1R w - - 0 1',
+  moves: [
+   'e5-g4',
+   'f6-g4',
+   'd1-g4'
+  ]
+ },   
+    
+      {
+  date: '11/12/2014',
+  balP: '1',
+  balM: '1',
+  title: 'White to move!',
+  start: 'r2q1rk1/pp2bppn/n1p1p1bp/3pP3/2PP4/P1NB1N1P/1PQ2PP1/R1B1R1K1 w - - 0 1',
+  moves: [
+   'd3-g6',
+   'f7-g6',
+   'c2-g6'
+  ]
+ }, 
+    
+    {
+  date: '12/12/2014',
+  balP: '1',
+  balM: '1',
+  title: 'White to move!',
+  start: 'r4rk1/2qn1pbp/2p3p1/p1P1p1B1/1p2P3/1B3Q1P/PP3PP1/3R1RK1 w - - 0 1',
+  moves: [
+   'g5-e7',
+   'f8-c8',
+   'f3-f7'
+  ]
+ }, 
+    
+      {
+  date: '13/12/2014',
+  balP: '3',
+  balM: '3',
+  title: 'Black to move!',
+  start: 'r4rk1/pQ2bppp/4pn2/8/2Bq4/P1N5/1P3PPP/R3R1K1 b - - 0 1',
+  moves: [
+   'e7-c5',
+   'c3-e4',
+   'f6-e4',
+   'e1-e4',
+   'd4-f2',
+   'g1-h1',
+   'a8-b8',
+   'b7-a6',
+   'b8-b2'
+  ]
+ },
+    
+    {
+  date: '20/12/2014',
+  balP: '3',
+  balM: '3',
+  title: 'Black to move!',
+  start: 'r2qk2r/1b2bppp/p3p3/2pnN3/3p1P2/1P1P3P/PBPQN1P1/R3K2R b - - 0 1',
+  moves: [
+   'd5-e3',
+   'e5-c4',
+   'e3-g2',
+   'e1-d1',
+   'g2-e3',
+   'd1-c1',
+   'e3-c4',
+   'd3-c4',
+   'b7-h1',
+  ]
+ },
+    
+    {
+  date: '20/12/2014',
+  balP: '1',
+  balM: '1',
+  title: 'White to move!',
+  start: 'r2q1rk1/ppp1bpp1/2n3bp/3pP1n1/3P4/P1NBBN1P/1PQ2PP1/R3K2R w - - 0 1',
+  moves: [
+   'd3-g6',
+   'g5-f3',
+   'g2-f3',
+   'f7-g6',
+   'c2-g6'
+  ]
+ }, 
+    
+    {
+  date: '26/12/2014',
+  balP: '1',
+  balM: '1',
+  title: 'White to move!',
+  start: 'r4r2/pp1q1pkp/3p2p1/1N2p3/2Pn2P1/1P5P/P3PPK1/2RQ1R2 w - - 0 1',
+  moves: [
+   'b5-d4',
+   'e5-d4',
+   'd1-d4'
+  ]
+ },
+    
+    {
+  date: '26/12/2014',
+  balP: '2',
+  balM: '2',
+  title: 'White to move!',
+  start: '3r4/p5kp/5pp1/3pr3/3R2P1/1P2P2P/P4PK1/3R4 w - - 0 1',
+  moves: [
+   'e3-e4',
+   'f6-f5',
+   'e4-d5'
+  ]
+ }  //#47 -> 20
+];
+
